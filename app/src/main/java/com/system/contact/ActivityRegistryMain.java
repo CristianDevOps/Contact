e466cd2c1c615a20083d7ae82d8c5cc5fa19eb90
cristian <andresdevops1@gmail.com>
2021-05-25 10:14:41 -0500

    Build Project Android Studio

---
+++ app/src/main/java/com/system/contact/ActivityRegistryMain.java
@@ -0,0 +1,14 @@
+package com.system.contact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityRegistryMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registry);
+    }
+}
